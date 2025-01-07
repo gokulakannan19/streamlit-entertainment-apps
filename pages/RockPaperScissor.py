9f6c0188a9962d5ce04700cb5174ebe20928fe74
gokulakannan19 <sgokulakannan19.jc@gmail.com>
2025-01-07 22:01:12 +0530

    Added some more UI components for rock paper scissor

--- pages/RockPaperScissor.py
+++ pages/RockPaperScissor.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from modules import backend
 
 st.title("Rock Paper Scissor")
 
@@ -8,4 +9,27 @@ with st.form(key="rock-form"):
     button = st.form_submit_button("Done")
 
     if button:
-        pass
+        computer_choice = backend.rock_paper_scissor(player_input)
+        st.info(f"computer_choice: {computer_choice}")
+        st.success("You win")
+
+col1, col2, col3 = st.columns([1.5, 0.5, 1.5])
+with col1:
+    with st.container():
+
+        st.subheader("Your Score")
+        st.markdown(f"""
+            <div style="background-color: #c8e6c9; padding: 10px; border-radius: 5px;">
+                Score: 95
+            </div>
+            """, unsafe_allow_html=True)
+
+with col3:
+    with st.container():
+
+        st.subheader("Computer Score")
+        st.markdown(f"""
+            <div style="background-color: #c8e6c9; padding: 10px; border-radius: 5px;">
+                Score: 95
+            </div>
+            """, unsafe_allow_html=True)
