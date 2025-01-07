5bbe41b383002e511e83ab721f1b65f9be41578f
gokulakannan19 <sgokulakannan19.jc@gmail.com>
2025-01-07 20:47:20 +0530

    Flames backend completed

--- pages/Flames.py
+++ pages/Flames.py
@@ -1,8 +1,14 @@
 import streamlit as st
+from modules import backend
 
 st.title("Flames")
 
 with st.form(key="input-form"):
     name1 = st.text_input("Enter your name: ")
     name2 = st.text_input("Enter your partner name: ")
-    st.form_submit_button("Put Flames")
+    button = st.form_submit_button("Put Flames")
+
+    if button:
+        result = backend.put_flames(name1, name2)
+        st.info(result)
+
